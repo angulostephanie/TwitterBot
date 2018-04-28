69dc4f88834ce9ed7d2fa67f94fe577b9d6bc724
Teagan Mucher <tjsmuch@gmail.com>
2018-04-28 15:34:45 -0700

    Update README, add .gitignore, add sheets function to tweets.py

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 ### Diversity and Equity Hackathon 
 A twitter bot that gives users information regarding LA issues
+
+# How to run locally
+1. Install virtualenv
+    - This is virtual environment to allow you to install python packages in a closed system
+    - In a terminal navigate to the project directory and type the following:
+        - pip install virtualenv
+            - Installs virtualenv to your local machine
+        - virtualenv myvenv
+            - This creates a folder called myvenv in your project directory
+            - Note: myvenv is just the name of your virtual environment. It can be named whatever you want.
+        - source myvenv/bin/activate
+            - You should now see this at the start of each line in your terminal: (myvenv) computer-name:~\
+
+2. Install libraries
+    - In your terminal, in the virtual environment in your project directory, type the following:
+        - pip install tweepy
+        - pip install --upgrade google-api-client
+
+3. Get API keys
+    - Stephanie do this
+
+4. Ask Teagan how to get Sheets API json credentials files
+    - Teagan to update this
+
+
+# IFTTT Account
+eaglerockbot@gmail.com
+
+# Twitter Account
+@gentriwhy
--- tweets.py
+++ tweets.py
@@ -1,5 +1,8 @@
+from __future__ import print_function
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
 import tweepy, time, sys
-import keys
 
 CONSUMER_KEY = keys.consumer_key
 CONSUMER_SECRET = keys.consumer_secret
@@ -9,6 +12,8 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+# access google sheets
+#
 
 def retweet(id):
     # example - 990335296129527808 (need to parse the link and get the number)
@@ -16,4 +21,33 @@ def retweet(id):
     tweet.retweet()
 
 
+# how to post tweets
+
 api.update_status("This is a test!")
+
+
+
+# sheets stuff
+# Setup the Sheets API
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+store = file.Storage('credentials.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = build('sheets', 'v4', http=creds.authorize(Http()))
+
+# Call the Sheets API
+SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms' # TODO: change
+RANGE_NAME = 'Class Data!A2:E'  # TODO: change
+result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
+                                             range=RANGE_NAME).execute()
+values = result.get('values', [])
+if not values:
+    print('No data found.')
+else:
+    print('Name, Major:')
+    for row in values:
+        # Print columns A and E, which correspond to indices 0 and 4.
+        print('%s, %s' % (row[0], row[4]))
+
